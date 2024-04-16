11e7003cac24e75e3807af26ff0503dcc5093bd2
KrisMoe <raini.night161@gmail.com>
2024-04-16 16:17:56 -0500

    Styled item header and buttons

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,7 +1,7 @@
 import styles from '../css/Buttons.module.css';
 export default function AddButton(props){
     return( <div>
-            <button className={styles.AddButton}> Add </button>
+            <button className={styles.addButton}> Add </button>
             </div>
     )
 
--- src/components/ItemHeader.jsx
+++ src/components/ItemHeader.jsx
@@ -1,7 +1,12 @@
+import styles from '../css/ItemHeader.module.css';
 export default function ItemHeader(props){
-    return( <div>
-        
-        
+    return( <div className={styles.headerContainer} >
+                <div className={styles.imageContainer} >
+                <h4 className={styles.title}>{props.title}</h4>
+                <img className={styles.image} src={props.src} alt={props.alt} ></img>
+                
+             
+                </div>
             </div>
     )
 
