a6c5579838a8d409556adb7d5a5a44fe78f71326
Owen Ratgen <ratge006@umn.edu>
2024-04-24 20:18:24 -0500

    1

--- src/components/ItemHeader.jsx
+++ src/components/ItemHeader.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom'
 import styles from '../css/ItemHeader.module.css';
-export default function ItemHeader({ name, image, alt }){
+
+export default function ItemHeader({ name, image, alt, editLink }){
+    const navigate = useNavigate()
     return <div className={styles.headerContainer} >
         <div className={styles.imageContainer} >
         <h4 className={styles.title}>{name}</h4>
         <img className={styles.image} src={image} alt={alt} ></img>
+        <button className={styles.editButton} onClick={()=>navigate(editLink)}>Edit</button>
         </div>
     </div>
 }
--- src/components/ItemHeaderEditable.jsx
+++ src/components/ItemHeaderEditable.jsx
@@ -1,11 +1,32 @@
 import styles from '../css/ItemHeader.module.css';
+import { ReactComponent as UploadImage }  from '../assets/upload.svg'
 
-export default function ItemHeaderEditable({ name, image, alt, updateName }){
+export default function ItemHeaderEditable({ name, image, alt, updateName, updateImage }){
+    async function uploadImage(e) {
+        if (e.target.files.length <= 0) return
+        const file = e.target.files.item(0);
+        console.log("Image selected", file)
+        if (file.size() > 10000000) {    // 10,000,000 bytes = 10 mb
+            console.alert("Image file is too large (> 10mb)!")
+            return
+        }
+        updateImage(file)
+    }
+
     return <div className={styles.headerContainer} >
         <div className={styles.imageContainer} >
-        <input className={styles.inputTitle} value={name} onInput={(e)=>updateName(e.target.value)}/>
-        <img className={styles.image} src={image} alt={alt} ></img>
-        <button className={styles.editButton}>Edit</button>
+            <input className={styles.inputTitle} 
+                value={name} onInput={(e)=>updateName(e.target.value)}
+                maxLength="50" />
+            <div className={styles.imageUpload}>
+                <label for="file-input">
+                    <img className={styles.image} src={image} alt={alt} ></img>
+                </label>
+                {/* Upload image */}
+                <UploadImage className={styles.uploadSvg}/>
+                <input id="file-input" type="file" accept="image/*" capture="environment"
+                    onChange={uploadImage} />
+            </div>
         </div>
     </div>
 }
